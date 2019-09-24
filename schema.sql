0a36f94bdad7f0ee47ad94dea7f28ae30579f6a2
Darrik Puetz <darrikpuetz@gmail.com>
2019-09-24 07:45:44 -0700

    still trying to fix a bug

--- schema.sql
+++ schema.sql
@@ -1,27 +1,60 @@
-DROP DATABASE IF EXISTS city_database;
+DROP TABLE IF EXISTS weather CASCADE;
+DROP TABLE IF EXISTS trails;
+DROP TABLE IF EXISTS movies;
+DROP TABLE IF EXISTS yelp;
+DROP TABLE IF EXISTS events;
+DROP TABLE IF EXISTS locations CASCADE;
 
-CREATE TABLE IF NOT EXISTS locations(
-
-    id SERIAL PRIMARY KEY,
-    search_query VARCHAR(255),
-    formatted_query VARCHAR(255),
-    latitude NUMERIC(10,7),
-    longtitude NUMERIC(10,7)
-
+CREATE TABLE locations (
+  id SERIAL PRIMARY KEY, 
+  search_query VARCHAR(255),
+  formatted_query VARCHAR(255),
+  latitude NUMERIC(10, 7),
+  longitude NUMERIC(10, 7)
 );
 
-CREATE TABLE IF NOT EXISTS weather(
-
-    forecast VARCHAR (255),
-    time NUMERIC (10,7)
+CREATE TABLE weather (
+  id SERIAL PRIMARY KEY,
+  forecast VARCHAR(255),
+  "time" VARCHAR(255), 
+  created_time VARCHAR(255),
+  location_id INTEGER NOT NULL,
+  FOREIGN KEY (location_id) REFERENCES locations (id)
 );
 
+CREATE TABLE movies (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(255),
+  overview TEXT,
+  average_votes NUMERIC(10,3),
+  total_votes INTEGER, 
+  image_url TEXT,
+  popularity NUMERIC(6, 3),
+  released_on VARCHAR(255),
+  created_time VARCHAR(255),
+  location_id INTEGER NOT NULL,
+  FOREIGN KEY (location_id) REFERENCES locations (id)
+);
 
-CREATE TABLE IF NOT EXISTS events(
+CREATE TABLE events (
+  id SERIAL PRIMARY KEY,
+  link VARCHAR(255),
+  "name" VARCHAR(255), 
+  event_date VARCHAR(255),
+  summary VARCHAR(255),
+  created_time VARCHAR(255),
+  location_id INTEGER NOT NULL,
+  FOREIGN KEY (location_id) REFERENCES locations (id)
+);
 
-    link VARCHAR(255),
-    name VARCHAR (255),
-    event_date VARCHAR(255),
-    summary VARCHAR(255)
-
+CREATE TABLE yelp (
+  id SERIAL PRIMARY KEY,
+  "name" VARCHAR(255),
+  image_url VARCHAR(255), 
+  price VARCHAR(255),
+  rating NUMERIC(3,2),
+  "url" VARCHAR(255),
+  created_time VARCHAR(255),
+  location_id INTEGER NOT NULL,
+  FOREIGN KEY (location_id) REFERENCES locations (id)
 );
--- server.js
+++ server.js
@@ -1,21 +1,20 @@
 'use strict'
 
 const express = require('express');
-const superagent = require('superagent');
-const app = express();
-const cors = require('cors');
-const PORT = process.env.PORT || 3000;
-const pg = require('pg');
-app.use(cors());
 require('dotenv').config();
-const client = new pg.Client(process.env.DATABASE_URL);
+const app = express();
 app.use(express.static('public'));
-
-
+const cors = require('cors');
+const superagent = require('superagent');
+const pg = require('pg');
+const PORT = process.env.PORT || 3000;
 let lat = 0;
 let lng = 0;
-
-
+let currentCity = '';
+app.use(cors());
+const DATABASE_URL = process.env.DATABASE_URL;
+const client = new pg.Client(DATABASE_URL);
+client.on('error', errHandler);
 
 function Locations(searchQuery, geoDataResults) {
   this.searchQuery = searchQuery;
@@ -24,7 +23,7 @@ function Locations(searchQuery, geoDataResults) {
   this.longitude = geoDataResults.geometry.location.lng;
   lat = this.latitude;
   lng = this.longitude;
-
+  currentCity = this.searchQuery;
 }
 
 
@@ -34,105 +33,208 @@ function Weather(whichDay) {
   console.log(this);
 }
 
-
-function Event(eventBriteStuff) {
-  this.link = eventBriteStuff.url;
-  this.name = eventBriteStuff.name.text;
-  this.event_date = new Date(eventBriteStuff.start.local).toDateString();
-  this.summary = eventBriteStuff.summary;
+function Restaurant(yelpData){
+  this.name = yelpData.name;
+  this.image_url = yelpData.image_url;
+  this.price = yelpData.price;
+  this.rating = yelpData.rating;
+  this.url = yelpData.url;
 }
 
+function Events(eventData) {
+  this.link = eventData.url;
+  this.name = eventData.name.text;
+  this.event_date = new Date(eventData.start.local).toDateString();
+  this.summary = eventData.summary;
 
-app.get('/location', (request, response) => {
-  try {
-    let searchQuery = request.query.data;
-    let geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${searchQuery}&key=${process.env.GEOCODE_API_KEY}`;
-    //SuperAgent Things Here
-    superagent.get(geocodeUrl)
-      .then((geocodeUrlStuff) => {
-        const locations = new Locations(searchQuery, geocodeUrlStuff.body.results[0]);
-        console.log(locations);
-        response.send(locations);
+}
 
-      })
+function Movie(movieData){
+  this.title = movieData.title;
+  this.overview = movieData.overview;
+  this.average_votes = movieData.vote_average;
+  this.total_votes = movieData.vote_count;
+  this.image_url = `https://image.tmdb.org/t/p/w500/${movieData.poster_path}`;
+  this.popularity = movieData.popularity;
+  this.released_on = movieData.release_date;
+}
 
-  } catch (error) {
-    errHandler(error, response);
-  }
-});
-
-
-app.get('/location', (request, response) => {
+function newLocationCheck (request, response) {
   let searchQuery = request.query.data;
   let sqlQuery = `SELECT * FROM locations WHERE search_query='${searchQuery}'`;
   client.query(sqlQuery)
     .then(queryResult => {
       console.log(queryResult);
       if (queryResult.rowCount > 0) {
+        currentCity = queryResult.rows[0].search_query;
+        lat = queryResult.rows[0].latitude;
+        lng = queryResult.rows[0].longitude;
         response.send(queryResult.rows[0])
+        console.log('got info from database');
       }
       else {
+        console.log('Making an API call');
         let url = `https://maps.googleapis.com/maps/api/geocode/json?address=${searchQuery}&key=${process.env.GEOCODE_API_KEY}`
-        superagent.get(url)
-          .then(superagentResults => {
-            let results = superagentResults.body.results[0];
-            let locations = new Locations(searchQuery, results);
-            let sql = `INSERT INTO locations (search_query, formatted_query, latitude, longitude) VALUES ($1, $2, $3, $4);`
-            let values = [searchQuery, locations.formattedQuery, lat, lng];
-            client.query(sql, values)
-              .catch(error => errHandler(error, response));
-            response.send(locations);
-          })
+        superagentGetLocation(url, searchQuery, response);
       }
     })
     .catch(err => errHandler(err, response));
-})
+}
 
+function superagentGetLocation(url, searchQuery, response){
+  superagent.get(url)
+    .then(superagentResults => {
+      let locations = newLocation(superagentResults, searchQuery)
+      sendSQLLocation(searchQuery, locations);
+      //send to database
+      response.send(locations);
+    })
+}
+function newLocation(superagentResults, searchQuery){
+  let results = superagentResults.body.results[0];
+  let locations = new Locations(searchQuery, results);
+  return locations;
+}
 
+function sendSQLLocation(searchQuery, locations){
+  let sql = 'INSERT INTO locations (search_query, formatted_query, latitude, longitude) VALUES ($1, $2, $3, $4);'
+  let values = [searchQuery, locations.formattedQuery, lat, lng];
+  client.query(sql, values)
+    .then(pgResults => {
+      console.log('Asking Google, sending data to database');
+    })
+    .catch(error => errHandler(error));
+}
 
-
+  
+  
+  
 //Weather query from Dark Sky
-app.get('/weather', (request, response) => {
-  try {
-    let darkSkyURL = `https://api.darksky.net/forecast/${process.env.DARKSKY_API_KEY}/${request.query.data.latitude},${request.query.data.longitude}`;
-    superagent.get(darkSkyURL)
-      .end((error, darkSkyURLStuff) => {
-        const currentWeather = makeWeather(darkSkyURLStuff.body);
-        response.status(200).send(currentWeather);
-      })
-  } catch (error) {
-    errHandler(error, response);
-  }
-});
-
-app.get('/events', (request, response) => {
-  try {
-    let eventBriteURL = `https://www.eventbriteapi.com/v3/events/search?location.longitude=${request.query.data.longitude}&location.latitude=${request.query.data.latitude}&expand=venue&token=${process.env.EVENTBRITE_API_KEY}`;
-    superagent.get(eventBriteURL)
-      .then((eventBriteData) => {
-        const eventBriteInfo = getEvents(eventBriteData.body.events);
-        response.send(eventBriteInfo);
-      });
-  } catch (error) {
-    errHandler(error, response);
-  }
-});
-
-function makeWeather(weatherText) {
-  return weatherText.daily.data.map(day => new Weather(day));
+function getWeather(request, response) {
+  let searchQuery = request.query.data;
+  console.log(searchQuery);
+  let url = `https://api.darksky.net/forecast/${process.env.WEATHER_API_KEY}/${lat},${lng}`
+  superagentWeather(url, searchQuery, response)
+}
+function superagentWeather(url, searchQuery, response){
+  superagent.get(url)
+    .then(superagentResults => {
+      response.status(200).send(newWeather(superagentResults, searchQuery));
+    })
+    .catch(err =>{
+      errHandler(err, response);
+    })
 }
 
-function getEvents(eventMap) {
-  let eventInput = eventMap.map(event => new Event(event));
-  return eventInput.splice(0, 20);
+function newWeather(superagentResults, searchQuery){
+  const weatherDataResults = superagentResults.body;
+  let weatherArray = weatherDataResults.daily.data.map(day => {
+    const weather = new Weather(searchQuery, day);
+    return weather;
+  })
+  return weatherArray;
 }
+
+function getEvents(request, response){
+  let url = `https://www.eventbriteapi.com/v3/events/search?location.longitude=${lng}&location.latitude=${lat}&expand=venue&token=${process.env.EVENT_API_KEY}`
+  superagentEvent(url, response)
+}
+
+function superagentEvent(url, response){
+  superagent.get(url)
+    .then(superagentResults => {
+      response.status(200).send(newEvent(superagentResults));
+    })
+    .catch(err =>{
+      errHandler(err, response);
+    });
+}
+
+function newEvent(superagentResults){
+  const eventResults = superagentResults.body;
+  const eventsArray = eventResults.events.slice(0,10).map(event => {
+    let theEvent = new Events (event);
+    return theEvent;
+  });
+  return eventsArray;
+}
+
+function getMovies(request, response){
+  let url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&language=en-US&query=${currentCity}&page=1&include_adult=false`;
+  superagentMovies(url, response);
+}
+
+function superagentMovies(url, response){
+  superagent.get(url)
+    .then(superagentResults => {
+
+      response.send(newMovie(superagentResults));
+    })
+    .catch(error => console.log(error));
+}
+
+function newMovie(superagentResults){
+  const movieResults = superagentResults.body;
+  const moviesArray = movieResults.results.slice(0, 10).map(movie =>{
+    let currentMovie = new Movie(movie);
+    return currentMovie;
+  })
+  return moviesArray;
+}
+  
 
 function errHandler(error, response) {
-  console.error(error);
-  response.status(500).send(error);
+  console.log(error);
+  const errorObj = {
+    status: 500,
+    text: 'An error with the database has occurred. Please try again.'
+  };
+  response.status(500).send(errorObj);
 }
+
+function getYelp(request, response){
+
+  const apiKey = process.env.YELP_API_KEY;
+  
+  const searchRequest = {
+    term:'restaurant',
+    location: currentCity
+  };
+  
+  client2.search(searchRequest).then(yelpAPIResults => {
+    response.send(newYelp(yelpAPIResults));
+  }).catch(e => {
+    console.log(e);
+  });
+}
+
+function newYelp(yelpAPIResults){
+  const yelpResult = yelpAPIResults.jsonBody.businesses;
+  const yelpArray = yelpResult.slice(0, 20).map(restaurant =>{
+    let currentRestaurant = new Restaurant(restaurant);
+    return currentRestaurant;
+  });
+  return yelpArray;
+}
+
+
+app.get('/location', newLocationCheck);
+app.get('/weather',getWeather);
+app.get('/movies', getMovies);
+app.get('/yelp', getYelp);
+app.get('/events', getEvents);
+
 app.use('*', (request, response) => response.status(404).send('Location does not exist pal'));
 
-client.connect()
 
-app.listen(PORT, () => console.log(`listening on ${PORT}`));
+
+client.connect()
+  .then(()=>{
+    app.listen(PORT, () => console.log(`listening on ${PORT}`));
+  })
+  .catch(error => errHandler(error));
+  
+process.on('unhandledRejection', (reason, p) => {
+  console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
+  // application specific logging, throwing an error, or other logic here
+});
